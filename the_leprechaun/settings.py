5d9844005032f6d624e0ea9f189bad356a0bfe9f
Алексей Нечепуренко <nec-alesha30@yandex.ru>
2024-06-10 18:22:13 +0300

    Connect PostgreSQL

--- the_leprechaun/settings.py
+++ the_leprechaun/settings.py
@@ -68,10 +68,15 @@ WSGI_APPLICATION = 'the_leprechaun.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
